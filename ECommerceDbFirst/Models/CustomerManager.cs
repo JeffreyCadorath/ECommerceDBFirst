f8f5adc321347c8f668a2bdadaf98c4d1456d4ec
Jeff Cadorath <JJcadorath@hotmail.com>
2019-08-13 14:07:50 -0500

    Add project files.

---
+++ ECommerceDbFirst/Models/CustomerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceDbFirst.Models
+{
+    public class CustomerManager
+    {
+        ECommerceDBEntities db = new ECommerceDBEntities();
+
+        public ICollection<Customer> GetCustomerInCity(string city)
+        {
+            var customerList = db.Customers.Where(x => x.City == city).ToList();
+            return customerList;
+        }
+    }
+}
